e1c173c16fac4a007e2f12e7108fabfa44dbb351
nikatati <nikatati96@gmail.com>
2024-03-22 16:27:51 +0200

    Added Banner

--- buy2/Screens/Products/ProductContainer.js
+++ buy2/Screens/Products/ProductContainer.js
@@ -10,6 +10,7 @@ import {
 import ProductList from "./ProductList";
 import SearchedProduct from "./SearchedProducts";
 import { Container, Icon, Input } from "native-base";
+import Banner from "../Shared/Banner";
 
 const data = require("../../assets/data/products.json");
 
@@ -55,18 +56,46 @@ const ProductContainer = () => {
       {focus == true ? (
         <SearchedProduct productsFiltered={productsFiltered} />
       ) : (
-        <View style={{ marginTop: 100 }}>
-          <FlatList
-            numColumns={2}
-            //horizontal
-            data={products}
-            renderItem={({ item }) => <ProductList key={item.id} item={item} />}
-            keyExtractor={(item) => item.name}
-          />
+        <View style={styles.container}>
+          <View>
+            <Banner />
+          </View>
+
+          <View style={styles.listContainer}>
+            <FlatList
+              numColumns={2}
+              //horizontal
+              data={products}
+              renderItem={({ item }) => (
+                <ProductList key={item.id} item={item} />
+              )}
+              keyExtractor={(item) => item.name}
+            />
+          </View>
         </View>
       )}
     </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 100,
+    flexWrap: "wrap",
+    backgroundColor: "gainsboro",
+  },
+  listContainer: {
+    marginTop: 200,
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-start",
+    flexWrap: "wrap",
+    backgroundColor: "gainsboro",
+  },
+  center: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default ProductContainer;

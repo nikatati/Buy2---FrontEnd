8b2f466f26126cb3a8d293bc5ae172e89db6364d
nikatati <nikatati96@gmail.com>
2024-07-15 10:46:53 +0300

    add total price+clear btn+ checkout btn to cart screen

--- buy2/Screens/Cart/Cart.js
+++ buy2/Screens/Cart/Cart.js
@@ -18,46 +18,70 @@ import * as actions from "../../Redux/Actions/cartActions";
 var { height, width } = Dimensions.get("window");
 
 const Cart = (props) => {
+  var total = 0;
+  props.cartItems.forEach((cart) => {
+    return (total += cart.product.price);
+  });
   return (
-    <ScrollView>
-      {props.cartItems.length ? (
-        <View>
-          <Text style={styles.h1}>Cart</Text>
-          {props.cartItems.map((data) => {
-            return (
-              <List.Item key={Math.random()} style={styles.listitem}>
-                <View style={styles.left}>
-                  <Image
-                    style={styles.image}
-                    resizeMode="contain"
-                    source={{
-                      uri: data.product.image
-                        ? data.product.image
-                        : "https://www.ormistonhospital.co.nz/wp-content/uploads/2016/05/No-Image.jpg",
-                    }}
-                  />
-                </View>
-                <View style={styles.left}>
-                  <Text style={styles.TextProductName}>
-                    {data.product.name}
-                  </Text>
-                </View>
-                <View style={styles.right}>
-                  <Text style={styles.TextProductPrice}>
-                    $ {data.product.price}
-                  </Text>
-                </View>
-              </List.Item>
-            );
-          })}
+    <View style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        {props.cartItems.length ? (
+          <View>
+            <Text style={styles.h1}>Cart</Text>
+            {props.cartItems.map((data) => {
+              return (
+                <List.Item key={Math.random()} style={styles.listitem}>
+                  <View style={styles.left}>
+                    <Image
+                      style={styles.image}
+                      resizeMode="contain"
+                      source={{
+                        uri: data.product.image
+                          ? data.product.image
+                          : "https://www.ormistonhospital.co.nz/wp-content/uploads/2016/05/No-Image.jpg",
+                      }}
+                    />
+                  </View>
+                  <View style={styles.left}>
+                    <Text style={styles.TextProductName}>
+                      {data.product.name}
+                    </Text>
+                  </View>
+                  <View style={styles.right}>
+                    <Text style={styles.TextProductPrice}>
+                      $ {data.product.price}
+                    </Text>
+                  </View>
+                </List.Item>
+              );
+            })}
+          </View>
+        ) : (
+          <View style={styles.emptyContainer}>
+            <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+              Looks like your cart is empty
+            </Text>
+            <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+              Add products to your cart to get started
+            </Text>
+          </View>
+        )}
+      </ScrollView>
+      <View style={styles.bottomContainer}>
+        <View style={styles.left}>
+          <Text style={styles.priceTotal}>${total}</Text>
         </View>
-      ) : (
-        <View style={styles.container}>
-          <Text>Looks like your cart is empty</Text>
-          <Text>Add products to your cart to get started</Text>
+        <View style={styles.right}>
+          <Button title="Clear" />
         </View>
-      )}
-    </ScrollView>
+        <View style={styles.right}>
+          <Button
+            title="Checkout"
+            onPress={() => props.navigation.navigate("Checkout")}
+          />
+        </View>
+      </View>
+    </View>
   );
 };
 
@@ -69,10 +93,17 @@ const mapStateToProps = (state) => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+    paddingBottom: 100, // ensures content doesn't overlap with bottom container
+  },
   image: {
-    width: "100%",
-    height: 250,
-    marginBottom: 200,
+    width: width / 2 - 20 - 70,
+    height: width / 2 - 40 - 50,
+    backgroundColor: "transparent",
   },
   contentContainer: {
     marginTop: 20,
@@ -83,9 +114,7 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontWeight: "bold",
     marginBottom: 40,
-    fontSize: 25,
     alignSelf: "center",
-    //color: "blue",
   },
   TextProductName: {
     fontSize: 16,
@@ -103,11 +132,13 @@ const styles = StyleSheet.create({
     position: "absolute",
     bottom: 0,
     left: 0,
+    width: "100%",
   },
   left: {
     flexDirection: "row",
     justifyContent: "flex-start",
     alignItems: "center",
+    flex: 1,
   },
   right: {
     flexDirection: "row",
@@ -115,14 +146,19 @@ const styles = StyleSheet.create({
     alignItems: "center",
     flex: 1,
   },
-  image: {
-    width: width / 2 - 20 - 70,
-    height: width / 2 - 40 - 50,
-    backgroundColor: "transparent",
-  },
   listitem: {
     alignItems: "center",
     justifyContent: "center",
+  },
+  priceTotal: {
+    fontSize: 18,
+    margin: 20,
+    color: "red",
+  },
+  emptyContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
   },
 });
 

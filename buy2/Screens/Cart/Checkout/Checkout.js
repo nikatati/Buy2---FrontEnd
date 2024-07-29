3caacea31457f4903a097e2aa124abcb75d43ceb
nikatati <nikatati96@gmail.com>
2024-07-29 12:52:32 +0300

    shipping page done

--- buy2/Screens/Cart/Checkout/Checkout.js
+++ buy2/Screens/Cart/Checkout/Checkout.js
@@ -1,12 +1,112 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, Button, TextInput, ScrollView } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 
-const CheckOut = () => {
+import Icon from "react-native-vector-icons/FontAwesome";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { connect } from "react-redux";
+import FormContainer from "../../Shared/Form/FormContainer";
+import Input from "../../Shared/Form/Input";
+
+const countries = require("../../../assets/data/countries.json");
+
+const CheckOut = (props) => {
+  const [orderItems, setOrderItems] = useState();
+  const [address, setAddress] = useState();
+  const [address2, setAddress2] = useState();
+  const [city, setCity] = useState();
+  const [zip, setZip] = useState();
+  const [country, setCountry] = useState();
+  const [phone, setPhone] = useState();
+
+  useEffect(() => {
+    setOrderItems(props.cartItems);
+
+    return () => {
+      setOrderItems();
+    };
+  }, []);
+
+  const handleCheckOut = () => {
+    let order = {
+      city,
+      country,
+      dateOredered: Date.now(),
+      orderItems,
+      phone,
+      shippingAddress1: address,
+      shippingAddress2: address2,
+      zip,
+    };
+
+    props.navigation.navigate("Payment", { order: order });
+  };
+
   return (
-    <View>
-      <Text>CheckOut works</Text>
-    </View>
+    <KeyboardAwareScrollView
+      viewIsInsideTabBar={true}
+      extraHeight={200}
+      enableOnAndroid={true}
+    >
+      <FormContainer title={"Shipping Address"}>
+        <Input
+          placeholder={"Phone"}
+          name={"phone"}
+          value={phone}
+          keyboardType={"numeric"}
+          onChangeText={(text) => setPhone(text)}
+        />
+        <Input
+          placeholder={"Shipping Address 1 "}
+          name={"ShippingAddress1"}
+          value={address}
+          onChangeText={(text) => setAddress(text)}
+        />
+        <Input
+          placeholder={"Shipping Address 2 "}
+          name={"ShippingAddress2"}
+          value={address2}
+          onChangeText={(text) => setAddress2(text)}
+        />
+        <Input
+          placeholder={"City "}
+          name={"city"}
+          value={city}
+          onChangeText={(text) => setCity(text)}
+        />
+        <Input
+          placeholder={"Zip Code "}
+          name={"zip"}
+          value={zip}
+          keyboardType={"numeric"}
+          onChangeText={(text) => setZip(text)}
+        />
+
+        <View style={{ marginVertical: 10 }}>
+          <Text>Select country:</Text>
+          <Picker
+            selectedValue={country}
+            onValueChange={(itemValue) => setCountry(itemValue)}
+          >
+            {countries.map((c) => {
+              return <Picker.Item key={c.code} label={c.name} value={c.name} />;
+            })}
+          </Picker>
+        </View>
+
+        <View style={{ width: "80%", alignItems: "center" }}>
+          <Button title="Confirm" onPress={handleCheckOut} />
+        </View>
+      </FormContainer>
+    </KeyboardAwareScrollView>
   );
 };
 
-export default CheckOut;
+const mapStateToProps = (state) => {
+  const { cartItems } = state;
+  return {
+    cartItems: cartItems,
+  };
+};
+
+export default connect(mapStateToProps)(CheckOut);
---
+++ buy2/Screens/Shared/Form/FormContainer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { ScrollView, Dimensions, StyleSheet, Text } from "react-native";
+
+var { width } = Dimensions.get("window");
+
+const FromContainer = (props) => {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>{props.title}</Text>
+      {props.children}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 30,
+    marginBottom: 400,
+    width: width,
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 30,
+  },
+});
+
+export default FromContainer;
